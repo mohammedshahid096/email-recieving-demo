da270674b3fc66d7d2815d15abc7413626179ccb
Mohammed shahid <84904302+mohammedshahid096@users.noreply.github.com>
2022-01-15 18:23:33 +0530

    Email file is uploaded

---
+++ email.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>email</title>
+        <style>
+            #container{
+                /* background-color:cyan; */
+                width:105%;
+                margin:  0 auto;
+                font-size: 35px
+            }
+            input{
+                font-size: 35px
+                
+            }
+        </style>
+        </html>
+        <body>
+           <div id="container">
+               
+               <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+               <br>
+                <pre>
+                    Email     : <input type="email" name="emailname" placeholder="example@gmail.com">
+                    *the email will revcive on this email only
+                    <br>   
+                    Subject   : <input type="text" name="subject">
+                    <br>
+                    Message   : <textarea name="message" id="" cols="40" rows="15"></textarea>
+                    <br>
+                                 <input type="submit" value="Send">   <input type="reset" value="clear">
+                    
+                </pre>
+                
+            </form>
+            
+        </div>
+        </body>
+    </head>
+
+    <?php 
+    $to = $_POST["emailname"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+     $from =$_POST["emailname"];
+     $headers ="From : $from";
+    //  echo $to." ".$subject." ".$message." ".$from;
+     $result= mail($to, $subject ,$message, $headers);
+     if($result){
+         echo "successfully send ";
+     }
+     else{
+         echo "failed to send";
+     }
+    ?>
